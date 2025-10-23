c5ef86437b068220e631d15e01334fa47b4251e1
Roberto Arcomano <bertolinux@gmai.com>
2025-10-24 01:08:03 +0200

    Added some new features

--- example1.py
+++ example1.py
@@ -3,15 +3,31 @@ import pandas as pd
 
 ps = pd.Series([1, 2, 3])
 print(ps)
+print()
 
-df = pd.DataFrame()
-df["Close"] = [1000, 2000, 3000]
+df = pd.DataFrame({"Close": [1000, 2000, 3000]})
 df["Open"] = [100, 200, 300]
 print(df)
+print()
 
-df1 = pd.DataFrame({"a": ps})
-df1["timestamp"] = pd.Timestamp("now")
-df1["n"] = 2
-print(df1)
-print(df1.dtypes)
+df["a"] = ps
+df["timestamp"] = pd.Timestamp("now")
+df["n"] = 2
+print(df)
+print()
+
+print(df.dtypes)
+print()
+
+print(df.head(1))
+print()
+
+print(df[0:2])
+print()
+
+print(df.loc[df["Open"]>100,"Open"])
+print()
+
+print(df.describe())
+print()
 
